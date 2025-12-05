42d252ac543e63123f54caa64e488bda19b60879
Electrolit1 <estivenpt2@gmail.com>
2025-12-05 00:58:56 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,17 @@
+// VERSION PARA USAR CON SCRIPT NORMAL EN HTML (SIN IMPORT)
 const firebaseConfig = {
-  apiKey: "TU_API",
-  authDomain: "TU_AUTH",
-  projectId: "TU_ID",
-  storageBucket: "TU BUCKET"
+  apiKey: "AIzaSyAbkpn2i02BQ4EdbxPRmjMTiJb_U32noZA",
+  authDomain: "respuestas-a0c38.firebaseapp.com",
+  databaseURL: "https://respuestas-a0c38-default-rtdb.firebaseio.com",
+  projectId: "respuestas-a0c38",
+  storageBucket: "respuestas-a0c38.firebasestorage.app",
+  messagingSenderId: "694006707121",
+  appId: "1:694006707121:web:79267ca0496b935bd88390",
+  measurementId: "G-FLNG6V9KMK"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firestore
 const db = firebase.firestore();
